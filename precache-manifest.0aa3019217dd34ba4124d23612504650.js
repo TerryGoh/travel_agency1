fc91363eb3ed75ea5bc979304824a3a7a96fb8af
TerryGoh <terrygoh.dev@gmail.com>
2021-08-25 12:12:15 +0800

    Updates

---
+++ precache-manifest.0aa3019217dd34ba4124d23612504650.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f92c8929cdf4efa59e6dcb44519b43d",
+    "url": "/travel_agency1/index.html"
+  },
+  {
+    "revision": "a3cebd00f9e621e8b644",
+    "url": "/travel_agency1/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "29d20b3868b4a743e209",
+    "url": "/travel_agency1/static/css/main.592595df.chunk.css"
+  },
+  {
+    "revision": "a3cebd00f9e621e8b644",
+    "url": "/travel_agency1/static/js/2.d0b0bd49.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/travel_agency1/static/js/2.d0b0bd49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d20b3868b4a743e209",
+    "url": "/travel_agency1/static/js/main.147c78b6.chunk.js"
+  },
+  {
+    "revision": "11c41241d648fdab0d4e",
+    "url": "/travel_agency1/static/js/runtime-main.5af6cb2b.js"
+  },
+  {
+    "revision": "e2e740f56592080d4b944e512081d2b4",
+    "url": "/travel_agency1/static/media/Default_Jumbotron.e2e740f5.jpg"
+  },
+  {
+    "revision": "77b5d8c3b7029c1f82711b72c93226ff",
+    "url": "/travel_agency1/static/media/Taiwan_1.77b5d8c3.png"
+  },
+  {
+    "revision": "51bcd34203718eb9c9488cbc20853b9c",
+    "url": "/travel_agency1/static/media/Taiwan_2.51bcd342.png"
+  },
+  {
+    "revision": "e989cd15a76018d6a3b806a28cbf4087",
+    "url": "/travel_agency1/static/media/Taiwan_3.e989cd15.png"
+  },
+  {
+    "revision": "b75a1fb52b004951b370132914e86e93",
+    "url": "/travel_agency1/static/media/Taiwan_4.b75a1fb5.png"
+  },
+  {
+    "revision": "a78f271f25016dd54d542d2ad9f29981",
+    "url": "/travel_agency1/static/media/Taiwan_Jumbotron.a78f271f.jpg"
+  },
+  {
+    "revision": "a9d2285ca9a0c55d51836adef37bad36",
+    "url": "/travel_agency1/static/media/Thai_1.a9d2285c.png"
+  },
+  {
+    "revision": "c8eca3c889f72c60a428a9e9d82cb203",
+    "url": "/travel_agency1/static/media/Thai_2.c8eca3c8.png"
+  },
+  {
+    "revision": "f602a7ae7783375c033cc43bef2f70c2",
+    "url": "/travel_agency1/static/media/Thai_3.f602a7ae.png"
+  },
+  {
+    "revision": "4aebc0d9f473d84d02f661c4e4f2a366",
+    "url": "/travel_agency1/static/media/Thai_Jumbotron.4aebc0d9.jpg"
+  },
+  {
+    "revision": "01dfa1d229c61b9b4fe1d4c473b44c25",
+    "url": "/travel_agency1/static/media/contact_us.01dfa1d2.jpg"
+  },
+  {
+    "revision": "5dc917f6e704dd77e22c1db1f9b478d1",
+    "url": "/travel_agency1/static/media/free_n_easy.5dc917f6.jpg"
+  },
+  {
+    "revision": "62a22de99a7350b69d37b6328132c1e8",
+    "url": "/travel_agency1/static/media/home.62a22de9.jpg"
+  },
+  {
+    "revision": "f88f52f7458f2c3f705128c13dfc5a90",
+    "url": "/travel_agency1/static/media/membership.f88f52f7.jpg"
+  },
+  {
+    "revision": "ead3c5bcc6c1f5ebe1df4b71d3c8a787",
+    "url": "/travel_agency1/static/media/website-under_construction.ead3c5bc.jpg"
+  }
+]);
